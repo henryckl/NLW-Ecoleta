c584079a7a780e1be917712cadcf92e0f20edd53
Henryck <henryck.leandro@gmail.com>
2020-06-01 19:34:31 -0300

    base da api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 
 const app = express()
-
+app.use(express.json())
 app.get('/users', (req, res) => {
   console.log('Listagem de usuarios')
 
-  res.send('Hello')
+  res.json({ message: 'Hello' })
 })
 
 app.listen(4000)
