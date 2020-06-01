4330c0a9de77fd8f7d4c11f5018a48ea5e3310f2
Henryck <henryck.leandro@gmail.com>
2020-06-01 17:57:45 -0300

    response send

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,10 @@ import express from 'express'
 
 const app = express()
 
-app.get('/user', () => {
+app.get('/users', (req, res) => {
   console.log('Listagem de usuarios')
+
+  res.send('Hello')
 })
 
 app.listen(4000)
