d3ac02f638b6b769df4c5ea81d7943a0efe50275
Henryck <henryck.leandro@gmail.com>
2020-06-02 19:26:19 -0300

    create migrations

---
+++ backend/knexfile.ts
@@ -0,0 +1,11 @@
+import { resolve } from 'path'
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: resolve(__dirname, 'src', 'database', 'database.sqlite')
+  },
+  migrations: {
+    directory: resolve(__dirname, 'src', 'database', 'migrations')
+  },
+  useNullAsDefault: true
+}
