98182eeedc0acb377eeac8e10a4e43f9a439f6f2
Henryck <henryck.leandro@gmail.com>
2020-06-02 20:52:30 -0300

    create point

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -8,11 +8,38 @@ routes.get('/items', async (req, res) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:4000/uploads/${item.image}`
     }
   })
   res.json(serializedItems)
+})
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = req.body
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  })
+
+  return res.json({ success: true })
 })
 
 export default routes
