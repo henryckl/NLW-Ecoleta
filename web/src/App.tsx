203e4351b9ca1260bfdd83837aa0b682fbb4aa69
Henryck <henryck.leandro@gmail.com>
2020-06-03 20:56:58 -0300

    createpage adicionada

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './pages/Home';
 
+import Routes from './routes';
 function App() {
   return (
-    <Header />
+    <Routes />
   );
 }
 
