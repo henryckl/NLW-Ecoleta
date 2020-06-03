450faf1a1621b80aa74cba0e86f1cb5bf5767d35
Henryck <henryck.leandro@gmail.com>
2020-06-02 21:12:13 -0300

    cadastro de ponto

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -28,7 +28,9 @@ routes.post('/points', async (req, res) => {
     items
   } = req.body
 
-  await knex('points').insert({
+  const trx = await knex.transaction()
+
+  const insertedIds = await trx('points').insert({
     image: 'image-fake',
     name,
     email,
@@ -38,6 +40,16 @@ routes.post('/points', async (req, res) => {
     city,
     uf
   })
+  const point_id = insertedIds[0]
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id
+    }
+  })
+
+  await trx('point_items').insert(pointItems)
 
   return res.json({ success: true })
 })
